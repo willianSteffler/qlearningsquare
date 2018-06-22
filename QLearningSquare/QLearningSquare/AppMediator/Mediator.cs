47ef4b627d491d7dd003034f3b5386ae4f0c8942
Willian <wsdorneles@ucs.br>
2018-06-22 12:01:20 -0300

    working on initial charge

--- QLearningSquare/QLearningSquare/AppMediator/Mediator.cs
+++ QLearningSquare/QLearningSquare/AppMediator/Mediator.cs
@@ -4,16 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QLearningSquare.DAO;
 
 namespace QLearningSquare.AppMediator
 {
+    public delegate void GlobalBusMessage(string message);
+
     class Mediator
     {
         private static Mediator mediator = new Mediator();
-        public GUIControl GUI = new GUIControl();
+        public List<GlobalBusMessage> GlobalBus = new List<GlobalBusMessage>();
+
+        //initialize classes
+        public GUIControl pGUI = new GUIControl();
+        public DataAcessObject pDAO = new DataAcessObject();
 
         internal static Mediator pMediator { get => mediator; }
-
-
     }
 }
---
+++ QLearningSquare/QLearningSquare/DAO/DataAcessObject.cs
@@ -0,0 +1,44 @@
+﻿using JsonMaker;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLearningSquare.DAO
+{
+    class DataAcessObject
+    {
+        JSON parameters = new JSON();
+
+        public bool loadParameters(string url)
+        {
+            try
+            {
+                string jsonString = File.ReadAllText(url);
+                parameters.fromJson(jsonString);
+                return true;
+            }
+            catch { }
+
+            return false;
+        }
+
+        public List<List<int>> getStateRewards()
+        {
+            List<List<int>> rewards = new List<List<int>>();
+            for (int i = 0; i < parameters.getChildsNames("StateRewards").Count; i++)
+            {
+                rewards[i] = new List<int>();
+                for(int j = 0; j < parameters.getChildsNames("StateRewards."+i).Count; j++)
+                {
+                    rewards[i][j] = parameters.getInt("StateRewards." + i + "." + j);
+                }
+            }
+
+            return rewards;
+        }
+
+    }
+}
