1f1e6a10d38a0a65b35035fbfe878148be20c79f
Willian Steffler <willian.steffler@inova.ind.br>
2018-06-26 13:44:45 -0300

    working on mainwindow viewmodel

--- QLearningSquare/QLearningSquare/MainWindow.xaml.cs
+++ QLearningSquare/QLearningSquare/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,18 +18,59 @@ using QLearningSquare.GUI;
 
 namespace QLearningSquare
 {
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public StateViewModel ActualStateInfo = new StateViewModel();
+        public ObservableCollection<StateViewModel> Qtable = new ObservableCollection<StateViewModel>();
+
+        private int animateInterval;
+        private bool autoAnimate;
+
+        
+        public int AnimateInterval { get => animateInterval;
+            set
+            {
+                if (animateInterval != value)
+                {
+                    animateInterval = value;
+                    RaisePropertyChanged("AnimateInterval");
+                }
+            }
+        }
+        public bool AutoAnimate { get => autoAnimate;
+            set
+            {
+                if (autoAnimate != value)
+                {
+                    autoAnimate = value;
+                    RaisePropertyChanged("AutoAnimate");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
 
     /// <summary>
     /// Interação lógica para MainWindow.xam
     /// </summary>
     public partial class MainWindow : Window
     {
+        public MainWindowViewModel ViewModel = new MainWindowViewModel();
         GUIControl ctrl = (GUIControl)AppMediator.Mediator.pMediator.pGUI;
 
         public MainWindow()
         {
             InitializeComponent();
             ctrl.pMainWindow = this;
+            this.DataContext = ViewModel;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
